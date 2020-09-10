b103af8d8691f6482f867fd9795dd48c20e68a44
naomi <iam.naomi92@gmail.com>
2020-09-10 21:55:50 +0900

    challenge11

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 // モーダル要素の取得
 const modal = document.getElementById('easyModal');
-console.log(modal);
 
 // 開くボタン取得
-const open = document.getElementById('modalOpen');
-console.log(open);
+const openBtn = document.getElementById('modalOpen');
 
 // 閉じるボタン取得
-const close = document.getElementsByClassName('modalClose')[0];
-console.log(close);
+const closeBtn = document.getElementsByClassName('modalClose')[0];
 
 // クリックで開くイベントリスナー
-open.addEventListener('click', modalOpen);
+openBtn.addEventListener('click', modalOpen);
 
 // クリックで閉じるイベントリスナー
-close.addEventListener('click', modalClose);
+closeBtn.addEventListener('click', modalClose);
 
 // モーダル以外の場所クリックのイベントリスナー
-//.addEventListener('click', clickAnywhere);
+window.addEventListener('click', clickAnywhere);
 
 // モーダルオープンの関数
+
 function modalOpen() {
-    
+    console.log(1);  
 }
 
 // モーダルを閉じる関数
 function modalClose() {
-
+    console.log(2);
 }
 
 // モーダル以外がクリックされた時に閉じる関数
 function clickAnywhere() {
-
+    console.log(3);
 }
